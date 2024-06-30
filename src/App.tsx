9f411e29553cca8f7084b3a8fed6922d6a5493d2
dominicmena <mena.dominic@gmail.com>
2024-06-30 13:27:59 -0500

    created an anonymous function that returns a random array value from the words list, console.log shows a random word every refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-
+import words from "./wordList.json"
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test")
-
-  return (
-    <>
-      <h1>hello world</h1>
-    </>
-  );
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+console.log(wordToGuess)
+  return <h1>hello world</h1>
 }
 
 export default App;
