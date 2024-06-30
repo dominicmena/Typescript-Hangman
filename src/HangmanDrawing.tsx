6d7e416a1fcc9790d49dd97c1fdc873a73bc4586
dominicmena <mena.dominic@gmail.com>
2024-06-30 14:15:52 -0500

    created HEAD variable with div for the head piece of hangman

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 
+const HEAD = (
+    <div style={{
+        width: "50px",
+        height: "50px",
+        borderRadius: "100%",
+        border: "10px solid black",
+        position: "absolute",
+        top: "50px",
+        right: "-30px"
+    }}/>
+)
+
 function HangmanDrawing() {
   return (
     <div style={{ position: "relative" }}>
+        {HEAD}
          <div
         style={{
           height: "50px",
