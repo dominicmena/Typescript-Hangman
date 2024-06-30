757c431f5714fcb0b13d36b5ac6b5a007a354512
dominicmena <mena.dominic@gmail.com>
2024-06-30 13:37:31 -0500

    created state for holding all the guessed letters in an array and a wrapper div with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
-import words from "./wordList.json"
+import words from "./wordList.json";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  })
-console.log(wordToGuess)
-  return <h1>hello world</h1>
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
+  return <div style={{
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin: "0 auto",
+    alignItems: "center"
+  }}></div>
 }
 
 export default App;
